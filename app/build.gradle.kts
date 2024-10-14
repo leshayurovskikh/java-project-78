c3b5c067b747f14eca0e339f4dbc6831dfa296bf
leshayu <alexei.yurovskikh@yandex.ru>
2024-10-14 21:02:14 +0500

    chekstyle12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    java
+    id("java")
     checkstyle
     jacoco
 }
@@ -7,38 +7,26 @@ plugins {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-
-    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
-    implementation ("info.picocli:picocli:4.7.6")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.11.3")
-    implementation ("org.apache.commons:commons-lang3:3.12.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required = true
-        csv.required = false
-        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
-    }
+tasks.jacocoTestReport { reports {
+    xml.required.set(true)
+    csv.required = false
+    html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
 }
-
-jacoco {
-    toolVersion = "0.8.11"
-    reportsDirectory = layout.buildDirectory.dir("reports/jacoco")
 }
